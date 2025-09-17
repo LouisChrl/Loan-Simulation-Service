fa368829d03a1d5c5475fb6b82243007b99eb3a3
LouisChrl <louis.charollais@gmail.com>
2025-09-17 16:34:29 +0800

    modified run.sh and stop.sh to run Camunda's web BPMN modeler and run test processes (zeebe and operate)

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-docker compose -f docker-compose.yml -f camunda/docker-compose-web-modeler.yaml up -d
+# External Communication Network
+docker network inspect loan_app_backend >/dev/null 2>&1 || docker network create loan_app_backend
+
+# Stack A : Loan Simulation Service
+docker compose --project-name loan --env-file ./.env -f docker-compose.yml up -d
+
+# Stack B : Camunda
+docker compose --project-name camunda --env-file ./camunda/.env -f camunda/docker-compose-web-modeler.yaml -f camunda/docker-compose-core.yaml up -d
--- stop.sh
+++ stop.sh
@@ -1,1 +1,5 @@
-docker compose -f docker-compose.yml -f camunda/docker-compose-web-modeler.yaml down
+# Stack A : Loan Simulation Service
+docker compose --project-name loan --env-file ./.env -f docker-compose.yml down
+
+# Stack B : Camunda
+docker compose --project-name camunda --env-file ./camunda/.env -f camunda/docker-compose-web-modeler.yaml -f camunda/docker-compose-core.yaml down
