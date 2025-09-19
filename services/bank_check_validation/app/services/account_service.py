2489b1bbafc21ed21273036ed3f574534a3999da
LouisChrl <louis.charollais@gmail.com>
2025-09-19 22:04:17 +0800

    Bank Check Validation : Added services/ folder + bank_service script to handle requests concerning the Bank table + account_service script to handle requests concerning the Account table

---
+++ services/bank_check_validation/app/services/account_service.py
@@ -0,0 +1,18 @@
+import re
+from sqlalchemy.orm import Session
+from orm.orm import Account, Bank
+
+def verifyAccountExistence(db: Session, account_number: str) -> bool:
+    return db.query(Account).filter(Account.account_number == account_number).first() is not None
+
+def verifyAccountBalance(db: Session, account_number: str, required_amount: float) -> bool:
+    acc = db.query(Account).filter(Account.account_number == account_number).first()
+    return bool(acc and acc.balance >= required_amount)
+
+def verifyBankPatternForAccount(db: Session, account_number: str) -> bool:
+    # Validate Account Number VS Bank Pattern
+    acc = db.query(Account).filter(Account.account_number == account_number).first()
+    if not acc:
+        return False
+    bank = db.query(Bank).filter(Bank.id == acc.bank_id).first()
+    return re.fullmatch(bank.account_number_validity_pattern, account_number) is not None
