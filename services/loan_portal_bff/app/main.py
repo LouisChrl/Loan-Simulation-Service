a11f99b5105bd2cfd2d92a411d72489592694768
LouisChrl <louis.charollais@gmail.com>
2025-09-19 14:58:53 +0800

    Loan Portal BFF : Added Mutations import to build the graphQL schema

--- services/loan_portal_bff/app/main.py
+++ services/loan_portal_bff/app/main.py
@@ -3,18 +3,19 @@ import uvicorn
 import strawberry
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
-from graph.schema import Query
+from graph.schema import Query, Mutation
 
 PORT = int(os.getenv("PORT"))
 
 # GraphQL Route and Schema
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema)
 
 # FastAPI app initialization
 app = FastAPI(title="loan_portal_bff")
 app.include_router(graphql_app, prefix="/graphql")
 
+# Healthchecks
 @app.get("/healthz")
 def health() -> dict:
     return {"status": "ok"}
