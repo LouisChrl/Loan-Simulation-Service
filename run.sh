2ae7594f3c8baa4ebb8617895b04cdcf282f75ca
LouisChrl <louis.charollais@gmail.com>
2025-09-25 18:05:54 +0800

    Added Sonarqube build in the run.sh script

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 # External Communication Network
 docker network inspect loan_app_backend >/dev/null 2>&1 || docker network create loan_app_backend
 
+# Optional : Sonarqube
+# docker run -d --name sonarqube -p 9000:9000 \
+#   -v sonarqube_data:/opt/sonarqube/data \
+#   -v sonarqube_extensions:/opt/sonarqube/extensions \
+#   -v sonarqube_logs:/opt/sonarqube/logs \
+#   sonarqube:community
+
 # Stack A : Loan Simulation Service
 docker compose --project-name loan --env-file ./.env -f docker-compose.yml up -d
 
